46496deb390e8d20e9c8ee1cdfe499d8020a7477
cody <cevek@ya.ru>
2019-07-11 08:15:45 +0000

    Add file uploading

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import * as https from 'http';
-import {readFileSync, readdir, readFile, stat} from 'fs';
+import {readFileSync, readdir, readFile, stat, createWriteStream} from 'fs';
 import * as Express from 'express';
 import * as mkdirp from 'mkdirp';
 import {promisify} from 'util';
 import {Exception, ClientException} from '.';
+import * as basicAuth from 'express-basic-auth';
+
 const readdirAsync = promisify(readdir);
 const statAsync = promisify(stat);
 
@@ -26,9 +28,6 @@ const config = {
 };
 
 mkdirp.sync(config.logDir);
-
-const projects = new Set(Object.keys(config.services));
-
 const server = https.createServer(
     {
         // key: readFileSync(config.https.key, 'utf8'),
@@ -38,25 +37,48 @@ const server = https.createServer(
 );
 server.listen(config.port);
 
-express.post('/sizes', Express.json(), async (req, res, next) => {
+const auth = basicAuth({
+    users: config.services,
+    unauthorizedResponse(req: basicAuth.IBasicAuthedRequest) {
+        throw new Exception('Credentials is incorrect', {auth: req.auth});
+    },
+});
+
+express.post('/sizes', auth, Express.json(), async (req, res, next) => {
     try {
-        const project: string = req.body.project;
         const files: string[] = req.body.files;
-        res.send(await getFileSizes(getProjectDir(project), files));
+        const {user} = (req as basicAuth.IBasicAuthedRequest).auth;
+        const dir = config.logDir + user + '/';
+        res.send(await getFileSizes(dir, files));
     } catch (err) {
         next(err);
     }
 });
 
-function getProjectDir(project: string) {
-    if (typeof project !== 'string') {
-        throw new Exception('Project is not specified');
+express.post('/upload', auth, async (req, res, next) => {
+    try {
+        const {file, fromPos} = req.query;
+        if (typeof file !== 'string') {
+            throw new Exception('file is not specified');
+        }
+        if (typeof fromPos !== 'string') {
+            throw new Exception('fromPos is not specified');
+        }
+        const {user} = (req as basicAuth.IBasicAuthedRequest).auth;
+        const projectDir = config.logDir + user + '/';
+        const size = await getFileSize(projectDir, file);
+        if (size !== +fromPos) {
+            throw new Exception('filePos is not correct', {size, fromPos});
+        }
+        const stream = createWriteStream(projectDir + file, {flags: 'a'});
+        req.pipe(stream);
+        req.on('end', () => {
+            res.send('ok');
+        });
+    } catch (err) {
+        next(err);
     }
-    if (!projects.has(project)) {
-        throw new Exception('Project is not exists', {project});
-    }
-    return config.logDir + project + '/';
-}
+});
 
 async function getFileSizes(dir: string, files: string[]) {
     if (!Array.isArray(files) || !files.every(file => typeof file === 'string')) {
@@ -64,22 +86,26 @@ async function getFileSizes(dir: string, files: string[]) {
     }
     const sizes: (number)[] = [];
     for (const file of files) {
-        if (!/^([\w_\-]+)\.log$/.test(file)) {
-            throw new Exception('incorrect filename', {file});
-        }
-        try {
-            const info = await statAsync(dir + file);
-            if (!info.isFile) {
-                throw new Exception('not a file', {file});
-            }
-            sizes.push(info.size);
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                sizes.push(0);
-            } else {
-                throw err;
-            }
-        }
+        sizes.push(await getFileSize(dir, file));
     }
     return sizes;
+}
+
+async function getFileSize(dir: string, file: string) {
+    if (!/^([\w_\-]+)\.log$/.test(file)) {
+        throw new Exception('incorrect filename', {file});
+    }
+    try {
+        const info = await statAsync(dir + file);
+        if (!info.isFile) {
+            throw new Exception('not a file', {file});
+        }
+        return info.size;
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            return 0;
+        } else {
+            throw err;
+        }
+    }
 }
---
+++ src/uploader.ts
@@ -0,0 +1,8 @@
+import * as https from 'http';
+
+
+
+
+https.request({
+    
+});
